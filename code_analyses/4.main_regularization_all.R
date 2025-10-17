cec8fbccb0a921abfc6e7f257466d924f4d0812c
Louise Laux <louise.laux@imev-mer.fr>
2025-10-17 08:54:16 +0000

    add the stratification index

--- code_analyses/4.main_regularization_all.R
+++ code_analyses/4.main_regularization_all.R
@@ -29,6 +29,10 @@ tablo <- read_tsv(file="data/tablo.tsv")
 
 hplc <- read_tsv("data/hplc_coeffs.tsv")
 
+# CTD
+
+final_point_B_ctd <- read_tsv("data/final_point_B_ctd.tsv")
+
 ###### Step 1: For plankton only, pre-clean and format to have it structured as the one of hydro #####
 
 df_plankton <- extract_wanted_level_from_tablo(tablo, level_col="family_like", value_col="concentration")
@@ -54,6 +58,31 @@ res_hplc <- run_to_clim(
   max_gap_interp = 3
 )
 
+
+# Regularize CTD
+
+ctd <- final_point_B_ctd %>%
+       mutate(date = ymd(date)) %>%
+       pivot_longer(cols = "stratif_index", names_to = "var", values_to = "value") %>%
+       mutate(depth =10) %>%
+       select(date, var, depth, value)
+
+ctd <- ctd %>%
+      dplyr::rename(name = var) %>%  
+      dplyr::mutate(depth = 10)
+
+# When to start the grid to make it match with plankton
+anchor <- as.Date("1967-01-04")
+grid_1995 <- seq(anchor, as.Date("1995-12-31"), by = "14 days")
+grid_1995 <- grid_1995[year(grid_1995) == 1995]
+
+
+res_ctd <- run_to_clim(
+  df_long=ctd,
+  start_date = as.Date("1995-01-18"),
+  by_days=14,
+  tolerance_days=3,
+  max_gap_interp=5)
 
 ##### Step 3 : Plot to confirm it worked as expected #####
 g_hydro_T_9395 <- plot_rf_series_2(res_std$df_long_rf,
@@ -132,3 +161,18 @@ res_plankton$debug_interp%>%mutate(year=year(target_date))%>%filter(year%in%c(20
 res_hplc$df_long_rf%>%mutate(year=year(target_date))%>%filter(year%in%c(2011:2023))%>%filter(var=="tchla")%>%select(target_date,var,season,obs,lin_value,final)
 
 
+# Save them in interm files
+
+# For the environment
+# Ctd
+final_panel_ctd <- res_ctd$final_panel_rf
+clim_wk <- res_ctd$clim_wk_smooth
+write_tsv(final_panel_ctd, file="data/interm_files/final_panel_ctd.tsv")
+write_tsv(clim_wk, file="data/interm_files/clim_wk_ctd.tsv")
+
+
+#Std 
+final_panel_std <- res_std$final_panel_rf
+clim_wk_std <- res_std$clim_wk_smooth
+write_tsv(final_panel_std, file="data/interm_files/final_panel_std.tsv")
+write_tsv(clim_wk_std, file="data/interm_files/clim_wk_std.tsv")
