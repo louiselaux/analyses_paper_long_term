e0e7febd66952629fe95cbf103baaebd134f0dbc
Louise Laux <louise.laux@imev-mer.fr>
2025-12-08 08:04:19 +0000

    add biovolumes as well

--- code_analyses/functions_pre_clean_plankton.R
+++ code_analyses/functions_pre_clean_plankton.R
@@ -51,3 +51,47 @@ fill_missing_dates_taxa <- function(df,
   
   return(df_filled)
 }
+
+
+##### To deal with biovolumes
+extract_biovol_from_tablo <- function(
+    tablo,
+    level_col = "family_like",   # family,order
+    depth     = 10,
+    shape     = c("ellipsoidal", "spherical")  # which one
+) {
+  shape <- match.arg(shape)
+  
+  tablo %>%
+    # 1. Convert 
+    dplyr::mutate(
+      area_mm2  = area * (0.0106)^2,
+      major_mm  = axis_major_length * 0.0106,
+      minor_mm  = axis_minor_length * 0.0106,
+      radius_mm = sqrt(area_mm2 / pi),
+      vol_ind   = dplyr::case_when(
+        shape == "spherical"  ~ (4/3) * pi * radius_mm^3,
+        shape == "ellipsoidal" ~ (4/3) * pi * (major_mm / 2) * (minor_mm / 2)^2,
+        TRUE ~ NA_real_
+      )
+    ) %>%
+    # 2. aggregation date per taxon
+    dplyr::group_by(
+      date  = as.Date(object_date),
+      name  = .data[[level_col]],
+      depth = depth
+    ) %>%
+    dplyr::summarise(
+      # nb objects with concentrations >0
+      n_pos  = sum(concentration > 0, na.rm = TRUE),
+      # biovolumes taken into account only if concentration >0 
+      value  = dplyr::if_else(
+        n_pos > 0,
+        sum(vol_ind * concentration, na.rm = TRUE),
+        NA_real_    # otherwise, concentration is NA
+      ),
+      .groups = "drop"
+    )
+}
+
+
